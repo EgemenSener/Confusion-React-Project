1ddc393f53154a3e7fb0b7da5eb4198eefe8b4a2
Egemen SENER <egemensener98@gmail.com>
2022-02-14 13:32:26 +0300

    dishDetail comp added and integreted!

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import DishDetail from './DishdetailComponent';
 
 class Menu extends Component {
 
@@ -15,24 +16,6 @@ class Menu extends Component {
         this.setState({ selectedDish: dish });
     }
 
-    renderDish(dish) {
-        if (dish != null) {
-            return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
-    }
     render() {
 
         const menu = this.props.dishes.map((dish) => {
@@ -55,7 +38,7 @@ class Menu extends Component {
 
                     {menu}
                     <div className='row'>
-                        {this.renderDish(this.state.selectedDish)}
+                        <DishDetail selectedDish = {this.state.selectedDish}/>
                     </div>
                 </div>
             </div>
