19c7c6eceb1bb2cbe1fde1aed1fd3aaa2c833dc7
Egemen SENER <egemensener98@gmail.com>
2022-02-26 20:20:57 +0300

    redux actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -25,8 +25,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Current State is:" + JSON.stringify(values));
-        alert("Current State is:" + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -102,7 +102,7 @@ class CommentForm extends Component {
     }
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
     if (comments !== null && comments.length !== 0) {
         const commentsList = comments.map(comment => {
             return (
@@ -124,7 +124,7 @@ function RenderComments({ comments }) {
             <div>
                 <h4>Comments</h4>
                 <ul className="list-unstyled">{commentsList}</ul>
-                <CommentForm />
+                <CommentForm dishId={dishId} addComment={addComment}/>
             </div>
         );
     } else {
@@ -165,7 +165,9 @@ const DishDetail = props => {
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+                        <RenderComments comments={props.comments}
+                        addComment={props.addComment} 
+                        dishId={props.dish.id}/>
                     </div>
                 </div>
             </div>
