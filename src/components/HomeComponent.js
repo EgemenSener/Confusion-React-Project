e60a7518f2c4af020cee967833fcbc236ee4420b
Egemen SENER <egemensener98@gmail.com>
2022-03-01 19:57:59 +0300

    fetch handling errors

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -31,11 +31,14 @@ function Home(props) {
         <div className='container'>
             <div className='row align-items-start'>
                 <div className='col-12 col-md m-1'>
-                    <RenderCard item={props.dish} isLoading={props.dishesLoading}
+                    <RenderCard item={props.dish}
+                        isLoading={props.dishesLoading}
                         errMess={props.dishesErrMess} />
                 </div>
                 <div className='col-12 col-md m-1'>
-                <RenderCard item={props.promotion} isLoading={props.promoLoading} errMess={props.promoErrMess} /> 
+                    <RenderCard item={props.promotion}
+                        isLoading={props.promoLoading}
+                        errMess={props.promoErrMess} />
                 </div>
                 <div className='col-12 col-md m-1'>
                     <RenderCard item={props.leader} />
