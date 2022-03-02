21fdfed6d842673b9fe394231ea0c9fc292c2c0c
Egemen SENER <egemensener98@gmail.com>
2022-03-02 11:26:47 +0300

    react animation components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -108,24 +109,28 @@ function RenderComments({ comments, postComment, dishId }) {
     if (comments !== null && comments.length !== 0) {
         const commentsList = comments.map(comment => {
             return (
-                <li>
-                    <p>{comment.comment}</p>
-                    <p>
-                        -- {comment.author},
-                        {new Intl.DateTimeFormat("en-US", {
-                            year: "numeric",
-                            month: "short",
-                            day: "2-digit"
-                        }).format(new Date(Date.parse(comment.date)))}
-                    </p>
-                </li>
+                <Fade in>
+                    <li>
+                        <p>{comment.comment}</p>
+                        <p>
+                            -- {comment.author},
+                            {new Intl.DateTimeFormat("en-US", {
+                                year: "numeric",
+                                month: "short",
+                                day: "2-digit"
+                            }).format(new Date(Date.parse(comment.date)))}
+                        </p>
+                    </li>
+                </Fade>
             );
         });
 
         return (
             <div>
                 <h4>Comments</h4>
-                <ul className="list-unstyled">{commentsList}</ul>
+                <Stagger in>
+                    <ul className="list-unstyled">{commentsList}</ul>
+                </Stagger>
                 <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
@@ -136,28 +141,36 @@ function RenderComments({ comments, postComment, dishId }) {
 
 function RenderDish({ dish }) {
     return (
-        <Card>
-            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-            <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-            </CardBody>
-        </Card>
+        <div className="col-12 com-md-5 m1">
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
+        </div>
     );
 }
 
 const DishDetail = props => {
     if (props.isLoading) {
-        return(
+        return (
             <div className="container">
                 <div className="row">
                     <Loading />
                 </div>
             </div>
-        ); 
+        );
     }
-    else if (props.errMess){
-        return(
+    else if (props.errMess) {
+        return (
             <div className="container">
                 <div className="row">
                     <h4>{props.errMess}</h4>
